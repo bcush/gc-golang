919f3c07dce86ba66ff03065712e078ea2c1473a
Brian Cushman <bcushman@corpinfo.com>
2017-08-16 08:38:16 -0700

    that's great

---
+++ 003-packages/cat/info.go
@@ -0,0 +1,15 @@
+package cat
+
+import (
+	"fmt"
+	"github.com/GoesToEleven/go-programming/code_samples/003-packages/cat"
+)
+
+func FromCat() {
+	fmt.Println("Hello from cat")
+	cat.FromCat()
+}
+
+func catEats() {
+	fmt.Println("asd")
+}
